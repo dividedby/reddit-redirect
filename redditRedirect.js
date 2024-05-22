13398e4a11218d234adbe2ce1d3b072e9391c537
dividedby <dividedbygit@gmail.com>
2024-05-22 17:11:16 -0500

    Improve script efficieny and update metadata keys

--- redditRedirect.js
+++ redditRedirect.js
@@ -3,28 +3,22 @@
 // @namespace   https://greasyfork.org/en/users/594496-divided-by
 // @author      dividedby
 // @description Redirects default frontpage from 'best' to 'hot' and entire site to new.reddit.com
-// @version     1.1
+// @version     1.2
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @contributionURL     https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=dividedbyerror@gmail.com&item_name=Reddit+Redirect+Donation
 // @contributionAmount  $1
 // @include     /^https:\/\/[a-z0-9.]*\.?reddit.com/
 // @run-at      document-start
-// @downloadURL https://update.greasyfork.org/scripts/495315/New%20Reddit%20Hot%20Redirect.user.js
-// @updateURL   https://update.greasyfork.org/scripts/495315/New%20Reddit%20Hot%20Redirect.user.js
+
 // ==/UserScript==
 
 (function () {
-  const currentUrl = window.location.href;
+  const currentHost = window.location.hostname;
+  const currentPath = window.location.pathname;
 
-  if (currentUrl.startsWith("https://www.reddit.com/")) {
-    // Redirect specific posts or subreddits to the new domain
-    const newUrl = currentUrl.replace(
-      "https://www.reddit.com",
-      "https://new.reddit.com"
-    );
-    window.location.replace(newUrl);
-  } else if (currentUrl.match(/^https?:\/\/[a-z0-9.]*reddit\.com\/?$/i)) {
-    // Redirect the frontpage to the hot section
-    window.location.replace(currentUrl + "hot");
+  if (currentHost === "www.reddit.com") {
+    window.location.replace(`https://new.reddit.com${currentPath}`);
+  } else if (currentHost.endsWith(".reddit.com") && currentPath === "/") {
+    window.location.replace(`${window.location.origin}/hot`);
   }
 })();
