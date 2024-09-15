e4becabac239d324bbc2fd11da28a6c44bfa3c66
dividedby <64715420+dividedby@users.noreply.github.com>
2024-09-14 21:22:50 -0700

    Update redditRedirect.js
    Update namespace

--- redditRedirect.js
+++ redditRedirect.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name        Reddit Hot Redirect with Logo Click Handler
+// @namespace   https://greasyfork.org/en/users/594496-divided-by
 // @author      dividedby
 // @description Redirects default frontpage from 'best' to 'hot' and handles logo clicks
-// @version     1.2
+// @version     1.3
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @contributionURL     https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=dividedbyerror@gmail.com&item_name=Reddit+Hot+Donation
 // @contributionAmount  $1
 // @match       https://www.reddit.com/*
 // @run-at      document-start
-// @namespace   https://greasyfork.org/users/162192
 
 // @downloadURL https://update.greasyfork.org/scripts/508344/Reddit%20Hot%20Redirect%20with%20Logo%20Click%20Handler.user.js
 // @updateURL https://update.greasyfork.org/scripts/508344/Reddit%20Hot%20Redirect%20with%20Logo%20Click%20Handler.meta.js
