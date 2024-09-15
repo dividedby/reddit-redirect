a070ed9185cf9d670defdac95b9fa6de3b26b625
dividedby <64715420+dividedby@users.noreply.github.com>
2024-09-14 20:17:34 -0700

    Update redditRedirect.js
    New script to only redirect to reddit.com/hot and when clicking on the home page logo

--- redditRedirect.js
+++ redditRedirect.js
@@ -1,24 +1,37 @@
 // ==UserScript==
-// @name        New Reddit Hot Redirect
-// @namespace   https://greasyfork.org/en/users/594496-divided-by
+// @name        Reddit Hot Redirect with Logo Click Handler
 // @author      dividedby
-// @description Redirects default frontpage from 'best' to 'hot' and entire site to new.reddit.com
-// @version     1.2
+// @description Redirects default frontpage from 'best' to 'hot' and handles logo clicks
+// @version     1.1
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @contributionURL     https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=dividedbyerror@gmail.com&item_name=Reddit+Redirect+Donation
+// @contributionURL     https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=dividedbyerror@gmail.com&item_name=Reddit+Hot+Donation
 // @contributionAmount  $1
-// @include     /^https:\/\/[a-z0-9.]*\.?reddit.com/
+// @match       https://www.reddit.com/*
 // @run-at      document-start
+// @namespace   https://greasyfork.org/users/162192
 
+// @downloadURL https://update.greasyfork.org/scripts/508344/Reddit%20Hot%20Redirect%20with%20Logo%20Click%20Handler.user.js
+// @updateURL https://update.greasyfork.org/scripts/508344/Reddit%20Hot%20Redirect%20with%20Logo%20Click%20Handler.meta.js
 // ==/UserScript==
 
-(function () {
-  const currentHost = window.location.hostname;
-  const currentPath = window.location.pathname;
+(function() {
+    'use strict';
 
-  if (currentHost === "www.reddit.com") {
-    window.location.replace(`https://new.reddit.com${currentPath}`);
-  } else if (currentHost.endsWith(".reddit.com") && currentPath === "/") {
-    window.location.replace(`${window.location.origin}/hot`);
-  }
+    if (location.pathname === '/') {
+        location.replace('https://www.reddit.com/hot');
+    }
+
+    const observer = new MutationObserver(() => {
+        const logo = document.querySelector('a[aria-label="Home"]');
+        if (logo && !logo.dataset.hotRedirect) {
+            logo.dataset.hotRedirect = true;
+            logo.addEventListener('click', e => {
+                e.preventDefault();
+                location.href = 'https://www.reddit.com/hot';
+            });
+            observer.disconnect();
+        }
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
 })();
